a16be4201b6e601d005dc601db2e62e266b28a35
Michal Czerwiak <s21356@pjwstk.edu.pl>
2022-04-09 17:13:33 +0200

    Zjazd 3 apka colors

---
+++ src/components/ColorList.js
@@ -0,0 +1,25 @@
+import React from "react";
+import Color from "./Color";
+export default function ColorList({
+                                      colors = [],
+                                      onRemoveColor = f => f,
+                                      onRateColor = f => f
+                                  }) {
+    if(!colors.length) return <div>No Colors Listed.</div>;
+    return (
+        <div className="color-list">
+            {
+                colors.map(color =>
+                    (
+                        <Color key={color.id}
+                               {...color}
+                               onRemove={onRemoveColor}
+                               onRate={onRateColor}
+                        />
+                    )
+                )
+            }
+        </div>
+    );
+}
+
---
+++ src/components/StarRating.js
@@ -0,0 +1,21 @@
+import React, { useState } from "react";
+import { FaStar } from "react-icons/fa";
+import Star from "./Star";
+const createArray = length => [...Array(length)];
+export default function StarRating({
+                                       totalStars = 5,
+                                       selectedStars = 0,
+                                       onRate = f => f
+                                   }) {
+    return (
+        <>
+            {createArray(totalStars).map((n, i) => (
+                <Star
+                    key={i}
+                    selected={selectedStars > i}
+                    onSelect={() => onRate(i + 1)}
+                />
+            ))}
+        </>
+    );
+}
